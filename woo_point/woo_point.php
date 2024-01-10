f02a769f9e5d9b626849c312770d253887c49db3
admin <lehuuduyen96@gmail.com>
2024-01-11 01:24:53 +0700

    update db

--- woo_point/woo_point.php
+++ woo_point/woo_point.php
@@ -160,15 +160,40 @@ register_activation_hook( __FILE__, 'pluginprefix_activate' );
 // 	'pluginprefix_deactivate'
 // );
 
-function my_custom_update_wc_order_status_function($order_id, $order, $status) {
+function my_custom_update_wc_order_status_function($order_id, $order) {
     // Check if the order type is 'shop_order'
+ 
     if ($order->get_type() === 'shop_order') {
         global $wpdb;
         $prefix = $wpdb->prefix;
         $history = $wpdb->get_results("SELECT * FROM ".$prefix."woo_history_user_point WHERE (order_id = '".$order_id."' AND status = '3')");
         if($history){
             $id= $history[0]->id;
+            $userId  = $order->data['customer_id'];
+            
+            
+            $totalOrder = $wpdb->get_results("SELECT SUM(total_order) as total FROM ".$prefix."woo_history_user_point WHERE (user_id = '".$userId."' AND status = '1')")[0]->total;
+            $totalOrder = ($totalOrder)?$totalOrder :0;
+            $checkRankBefore = $wpdb->get_results("SELECT * FROM ".$prefix."woo_rank WHERE (minimum_spending <= '".$totalOrder."') ORDER BY minimum_spending DESC LIMIT 1");
+
             $wpdb->query($wpdb->prepare("UPDATE ".$prefix."woo_history_user_point SET status=1 WHERE id=$id"));
+            $totalOrder = $wpdb->get_results("SELECT SUM(total_order) as total FROM ".$prefix."woo_history_user_point WHERE (user_id = '".$userId."' AND status = '1')")[0]->total;
+            $totalOrder = ($totalOrder)?$totalOrder :0;
+            $checkRankAfter = $wpdb->get_results("SELECT * FROM ".$prefix."woo_rank WHERE (minimum_spending <= '".$totalOrder."') ORDER BY minimum_spending DESC LIMIT 1");
+            if($checkRankBefore && $checkRankAfter && $checkRankBefore[0]->id != $checkRankAfter[0]->id){
+                $date = date('Y-m-d H:i:s');
+                $code = generateRandomString(8);
+                $priceSaleOff = $checkRankAfter[0]->price_sale_off;
+                $text = 'Voucher cho '.$checkRankAfter[0]->name.'. Ưu đãi '.$priceSaleOff;
+                $addVoucher = $wpdb->query($wpdb->prepare("INSERT INTO ".$prefix."posts (`post_author`, `post_date`, `post_date_gmt`, `post_title`, `post_excerpt`, `post_status`, `comment_status`, `ping_status`, `post_name`, `post_modified`, `post_modified_gmt`, `post_parent`, `post_type`) VALUES ('$userId','$date','$date','$code','$text','publish','closed','closed','$code','$date','$date','0','shop_coupon')"));
+                $PostIdVoucher = $wpdb->insert_id;
+                
+                $arrayEmail = serialize([$order->data['billing']['email']]);
+                $sqlAddMeta = "INSERT INTO ".$prefix."postmeta ( `post_id`, `meta_key`, `meta_value` ) VALUES ('$PostIdVoucher', 'discount_type', 'fixed_cart'), ('$PostIdVoucher', 'coupon_amount', '$priceSaleOff'), ('$PostIdVoucher', 'usage_limit', '1'), ('$PostIdVoucher', 'usage_limit_per_user', '1'), ('$PostIdVoucher', 'limit_usage_to_x_items', '0'), ('$PostIdVoucher', 'usage_count', '0'), ('$PostIdVoucher', 'customer_email', '$arrayEmail'), ('$PostIdVoucher', 'customer_user', '$userId')";
+                $addMeta = $wpdb->query($wpdb->prepare($sqlAddMeta));
+            }
+            
+            
         }
         // Your custom code to update something based on the WooCommerce order status change
 
@@ -176,4 +201,13 @@ function my_custom_update_wc_order_status_function($order_id, $order, $status) {
 }
 add_action('woocommerce_order_status_completed', 'my_custom_update_wc_order_status_function', 10, 4);
 
-// add_action('woocommerce_order_status_processing', 'my_custom_update_wc_order_status_function', 10, 4);
+// add_action('woocommerce_order_status_processing', 'my_custom_update_wc_order_status_function', 10, 3);
+function generateRandomString($length = 10) {
+    $characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[random_int(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
