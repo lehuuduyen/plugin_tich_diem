190abc7fd59389b27219d58f9f51cc4c321fb016
Ton Nguyen Hoang An <an.tonnguyenhoang@gmail.com>
2024-01-13 23:01:47 +0700

    Commit update plugin tich-diem and add plugin affliate

---
+++ woo_affliate/assets/scripts/affliate-scripts.js
@@ -0,0 +1,43 @@
+const _hiddenClass = 'd-none';
+const _activeClass = 'active';
+
+window.addEventListener('load', function() {
+  /* Tabs */
+  const _sPageURL = window.location.search.substring(1);
+  const _params = _sPageURL.split('&');
+  
+  const tabSetting1 = document.getElementById('tabSetting1');
+  const tabSetting2 = document.getElementById('tabSetting2');
+  const tabSetting1Content = document.getElementById('tab-setting-1-content');
+  const tabSetting2Content = document.getElementById('tab-setting-2-content');
+
+  if (tabSetting1) {
+    if (_params.length === 1 || _params[2].split('=')[1] === 'setting1') {
+      tabSetting1.classList.add(_activeClass);
+      tabSetting2.classList.remove(_activeClass);
+      tabSetting1Content.classList.add(_activeClass);
+      tabSetting2Content.classList.remove(_activeClass);
+    } else if (_params[2].split('=')[1] === 'setting1') {
+      tabSetting1.classList.remove(_activeClass);
+      tabSetting2.classList.add(_activeClass);
+      tabSetting1Content.classList.remove(_activeClass);
+      tabSetting2Content.classList.add(_activeClass);
+    }
+  }
+  
+  const _tabs = document.querySelectorAll('ul.nav-tabs-affliate > li');
+  for (i = 0; i < _tabs.length; i++) {
+    _tabs[i].addEventListener('click', _switchTab);
+  }
+
+  function _switchTab(event) {
+    event.preventDefault();
+    document.querySelector('ul.nav-tabs-affliate li.active').classList.remove(_activeClass);
+    document.querySelector('.tab-pane-affliate.active').classList.remove(_activeClass);
+    const clickedTab = event.currentTarget;
+    const anchor = event.target;
+    const activePaneID = anchor.getAttribute('href');
+    clickedTab.classList.add(_activeClass);
+    document.querySelector(activePaneID).classList.add(_activeClass);
+  }
+});
---
+++ woo_affliate/templates/admin.php
@@ -0,0 +1,28 @@
+<div class="wrap">
+  <h1>Hoa Hồng</h1>
+  <hr />
+  <ul class="nav-tabs-affliate">
+    <li id="tabSetting1" class="active">
+      <a href="#tab-setting-1-content">Setting 1</a>
+    </li>
+    <li id="tabSetting2">
+      <a href="#tab-setting-2-content">Setting 2</a>
+    </li>
+  </ul>
+  <div class="tab-content">
+    <div id="tab-setting-1-content" class="tab-pane-affliate active">
+      <h3>Form 1</h3>
+      <form action="" method="POST">
+        <input type="text" class="regular-text" />
+        <button class="button button-primary">Submit</button>
+      </form>
+    </div>
+    <div id="tab-setting-2-content" class="tab-pane-affliate">
+      <h3>Form 2</h3>
+      <form action="" method="POST">
+        <input type="text" class="regular-text" />
+        <button class="button button-primary">Submit</button>
+      </form>
+    </div>
+  </div>
+</div>
