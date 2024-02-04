1e46a233f48d6ef824099c9c727e5424c5d9af30
admin <lehuuduyen96@gmail.com>
2024-02-04 12:52:10 +0700

    update

--- woo_affliate/woo_affliate.php
+++ woo_affliate/woo_affliate.php
@@ -117,8 +117,17 @@ function aff_update_wc_order_status_function($order_id, $order) {
       $prefix = $wpdb->prefix;
       $history = $wpdb->get_results("SELECT * FROM ".$prefix."woo_history_user_commission WHERE (order_id = '".$order_id."' AND status = '3')");
       if($history){
+        
           $id= $history[0]->id;
-          $wpdb->query($wpdb->prepare("UPDATE ".$prefix."woo_history_user_commission SET status=1 WHERE id=$id"));
+          $userId= $history[0]->user_id;
+          $traffic = $wpdb->get_results("SELECT * FROM ".$prefix."woo_history_share_link WHERE (user_id = '".$userId."' AND status = '1') ORDER BY id DESC limit 1");
+          if($traffic){
+            $trafficId = $traffic[0]->id;
+            $wpdb->query($wpdb->prepare("UPDATE ".$prefix."woo_history_share_link SET status=0 WHERE user_id=$userId"));
+            $wpdb->query($wpdb->prepare("UPDATE ".$prefix."woo_history_share_link SET status=2 WHERE id=$trafficId"));
+            $wpdb->query($wpdb->prepare("UPDATE ".$prefix."woo_history_user_commission SET status=1 WHERE id=$id"));
+
+          }
       }
       // Your custom code to update something based on the WooCommerce order status change
 
